8ed92b87277b67d99b8925e9e2487796f2fcd304
nidakul <nidakul@hotmail.com>
2023-10-23 22:41:40 +0300

    End of conditionals

---
+++ Conditionals/Program.cs
@@ -0,0 +1,52 @@
+﻿var number = 10;
+//if (number == 10)
+//{
+//    Console.WriteLine("Number is 10");
+//}else if(number==20){
+//    Console.WriteLine("Number is 20");
+//}
+//else
+//{
+//    Console.WriteLine("Number is not 10 or 20");
+//}
+
+///* ------------------- */
+
+//Console.WriteLine(number==10 ? "Number is 10" : "Number is not 10");
+
+//switch (number)
+//{
+//    case 10:
+//        Console.WriteLine("Number is 10");
+//        break;
+//    case 20:
+//        Console.WriteLine("Number is 20");
+//        break;
+//    default:
+//        Console.WriteLine("Number is not 10 or 20");
+//        break;
+//}
+
+//if (number >= 0 && number<=100)
+//{
+//    Console.WriteLine("Number is between 0-100");
+//}
+//else if(number>100 && number <= 200)
+//{
+//    Console.WriteLine("Number is between 101-200");
+
+//}
+//else if(number > 200 || number < 0)
+//{
+//    Console.WriteLine("Number is less than 0 or greater than 200");
+//}
+//Console.ReadLine();
+
+if (number<100)
+{
+    if (number >= 90 && number < 95) 
+    {
+        Console.WriteLine("Number is equal to and greater than 90 and less than 95");
+    }
+
+}
