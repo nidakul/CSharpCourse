e8b423f18f8037677a7da80151a3ae365acc119a
nidakul <nidakul@hotmail.com>
2023-10-24 19:38:39 +0300

    End of findPrimeNumber

---
+++ FindPrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿if (isPrimeNumber(13))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is not a prime number");
+}
+
+static bool isPrimeNumber(int number)
+{
+    bool result = true;
+
+    for (int i = 2; i < number-1; i++)
+    {
+        if (number % i == 0)
+        {
+            result =  false;
+            i = number;
+        }
+        
+    }
+    return result;
+}
+
