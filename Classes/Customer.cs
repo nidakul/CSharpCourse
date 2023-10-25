669478d6cc068938118c86fa9fd506cbafd0c0d4
nidakul <nidakul@hotmail.com>
2023-10-25 23:52:02 +0300

    End of classes

---
+++ Classes/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Classes
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+
+		private string _firstName;
+		public string FirstName { get { return "Mr." + _firstName; } set { _firstName = value; } }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
+
---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Classes;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Customer customer = new Customer();
+customer.Id = 1; 
+customer.City = "Ankara";
+customer.FirstName = "Nida";
+customer.LastName = "Kul";
+
+Customer customer2 = new Customer
+{
+    Id = 2,
+    City = "İstanbul",
+    FirstName = "Engin",
+    LastName = "Demiroğ"
+};
+
+Console.WriteLine(customer2.FirstName);
