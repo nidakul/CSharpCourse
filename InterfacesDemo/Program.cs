7bff05ea97bfe151c17e911b4bfa0ecc8990921f
nidakul <nidakul@hotmail.com>
2023-10-27 09:21:34 +0300

    End of interfacesDemo

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,82 @@
+﻿
+IWorker[] workers = new IWorker[3]
+{
+    new Manager(),
+    new Worker(),
+    new Robot()
+};
+
+foreach (var worker in workers)
+{
+    worker.Work();
+}
+
+IEat[] eaters = new IEat[2]
+{
+    new Manager(),
+    new Worker()
+};
+
+foreach (var eat in eaters)
+{
+    eat.Eat();
+}
+
+interface IWorker
+{
+    void Work();
+}
+
+interface IEat
+{
+    void Eat();
+}
+
+interface ISalary
+{
+    void GetSalary();
+}
+
+class Manager : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        Console.WriteLine("Manager Eat");
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        Console.WriteLine("Manager Work");
+    }
+}
+
+class Worker : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        Console.WriteLine("Worker Eat");
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        Console.WriteLine("Worker Work");
+    }
+}
+
+class Robot : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot Work");
+    }
+}
