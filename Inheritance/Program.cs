cd484b80a5e8ed662c1c2c8cca6c0a8b9bf4e092
nidakul <nidakul@hotmail.com>
2023-10-27 09:30:42 +0300

    End of inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿Person[] persons = new Person[3]
+{
+    new Person {FirstName = "Nida"}, new Customer {FirstName = "Ela"}, new Student{FirstName = "Zehra"}
+};
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Departmant { get; set; }
+}
