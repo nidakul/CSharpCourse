70e35575b577b446330839ef88a7322ffba9c176
nidakul <nidakul@hotmail.com>
2023-10-27 09:47:38 +0300

    End of recapDemo

---
+++ RecapDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿//for Mac
+
+int[,] board = new int[8, 8];
+
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        if ((i + j) % 2 == 0)
+        {
+            board[i, j] = 1;
+        }
+        else
+        {
+            board[i, j] = 2;
+        }
+    }
+}
+
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        if (board[i, j] == 1)
+        {
+            Console.Write("W");
+        }
+        else
+        {
+            Console.Write("B");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
