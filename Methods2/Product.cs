d495cda0bd82ffda0731912cc776869487e1ca3a
nidakul <nidakul@hotmail.com>
2023-11-09 22:02:37 +0300

    End of Methods2

--- Methods2/Product.cs
+++ Methods2/Product.cs
@@ -7,6 +7,7 @@ namespace Methods2
 		public string Name { get; set; }
 		public double Price { get; set; }
 		public string Description { get; set; }
+		public int UnitInStock { get; set; }
 	}
 }
 
