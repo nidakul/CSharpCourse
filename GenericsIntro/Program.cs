4a0f62d20adfb661bfdb55cc78cb9c4549c3a62f
nidakul <nidakul@hotmail.com>
2023-11-10 10:37:11 +0300

    End of Day-4

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericsIntro;
+
+MyList<string> names = new MyList<string>();
+names.Add("Nida");
+
+Console.WriteLine(names.Length);
+
+names.Add("Engin");
+
+Console.WriteLine(names.Length);
+
+foreach (var item in names.Items)
+{
+    Console.WriteLine(item);
+}
